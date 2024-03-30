60a337d199bfa2ae77bce65cecc8533ec7567608
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-03-30 15:25:26 -0300

    Adicionando versão inicial do programa de simulação de distância de ping.

---
+++ ping_distance_simulation.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ping3
+
+# Endereço IP do dispositivo a ser pingado
+ip_address = ""
+
+# # Define uma sequência de alcances em metros , necessário por o disposito alinhado com a representação conceitual (exemplo : 1 há 10 metros)
+alcances = np.arange(1, 11, 1)  
+
+# Lista para armazenar os tempos de resposta do ping
+ping_times = []
+
+# Pingar o dispositivo em cada alcance e armazenar os tempos de resposta
+for alcance in alcances:
+    ping_time = ping3.ping(ip_address, timeout=1, unit="ms")
+    if ping_time is not None:
+        ping_times.append(ping_time/100)
+    else:
+        ping_times.append(0)  # se o ping falhar, assumimos que o tempo de resposta é 0
+
+# Plotar o gráfico de sinal da rede em função do alcance
+
+#Plotar gráfico de linha com Marcadores
+plt.plot(alcances, ping_times, marker='o')
+plt.title('Sinal da Rede em Função do Alcance')
+plt.xlabel('Alcance (metros)')
+plt.ylabel('Tempo de Resposta do Ping (ms)')
+plt.grid(True)
+plt.show()
+
+# Plotar um gráfico de barras
+plt.bar(alcances, ping_times)
+plt.title('Sinal da Rede em Função do Alcance')
+plt.xlabel('Alcance (metros)')
+plt.ylabel('Tempo de Resposta do Ping (ms)')
+plt.grid(True)
+plt.show()
+
