668b656879b3715159f34987945653da0efcd973
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-04-10 13:57:13 -0300

    Alterando um comentario , especificando o porque da mudança de escala

--- ping_distance_simulation.py
+++ ping_distance_simulation.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import ping3
 
 # Endereço IP do dispositivo a ser pingado
-ip_address = ""
+ip_address = "192.168.182.6"
 
 # # Define uma sequência de alcances em metros , necessário por o disposito alinhado com a representação conceitual (exemplo : 1 há 10 metros)
 alcances = np.arange(1, 11, 1)  
@@ -15,7 +15,7 @@ ping_times = []
 for alcance in alcances:
     ping_time = ping3.ping(ip_address, timeout=1, unit="ms")
     if ping_time is not None:
-        ping_times.append(ping_time/100)
+        ping_times.append(ping_time/100) # A resposta é entregue em número inteiro em ms , necessário alterar a escala(ms) para ter um melhor feedbeck.
     else:
         ping_times.append(0)  # se o ping falhar, assumimos que o tempo de resposta é 0
 
@@ -25,7 +25,7 @@ for alcance in alcances:
 plt.plot(alcances, ping_times, marker='o')
 plt.title('Sinal da Rede em Função do Alcance')
 plt.xlabel('Alcance (metros)')
-plt.ylabel('Tempo de Resposta do Ping (ms)')
+plt.ylabel('Tempo de Resposta do Ping (segundos)')
 plt.grid(True)
 plt.show()
 
@@ -33,7 +33,7 @@ plt.show()
 plt.bar(alcances, ping_times)
 plt.title('Sinal da Rede em Função do Alcance')
 plt.xlabel('Alcance (metros)')
-plt.ylabel('Tempo de Resposta do Ping (ms)')
+plt.ylabel('Tempo de Resposta do Ping (segundos)')
 plt.grid(True)
 plt.show()
 
