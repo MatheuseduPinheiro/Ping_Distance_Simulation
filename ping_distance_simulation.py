47adbaf5ff88899c0f08fde1fd1ee47b541514bb
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-04-11 01:06:24 -0300

    Corrigindo um erro lógico , o ping/100 estava viciando o dado

--- ping_distance_simulation.py
+++ ping_distance_simulation.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import ping3
 
 # Endereço IP do dispositivo a ser pingado
-ip_address = "192.168.182.6"
+ip_address = ""
 
 # # Define uma sequência de alcances em metros , necessário por o disposito alinhado com a representação conceitual (exemplo : 1 há 10 metros)
 alcances = np.arange(1, 11, 1)  
@@ -15,7 +15,7 @@ ping_times = []
 for alcance in alcances:
     ping_time = ping3.ping(ip_address, timeout=1, unit="ms")
     if ping_time is not None:
-        ping_times.append(ping_time/100) # A resposta é entregue em número inteiro em ms , necessário alterar a escala(ms) para ter um melhor feedbeck.
+        ping_times.append(ping_time) # A resposta é entregue em número inteiro(ms)
     else:
         ping_times.append(0)  # se o ping falhar, assumimos que o tempo de resposta é 0
 
